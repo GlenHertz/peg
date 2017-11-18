b743359293ff0a2ce7d89d907d1929c47cf625de
Glen Hertz <glen.hertz@gmail.com>
2017-11-18 12:24:45 -0500

    When match found only print out match

--- peg.nim
+++ peg.nim
@@ -6,8 +6,12 @@ let
   p = peg(pattern)
   filenames = args[1..^1]
 var
-  matches: seq[string] = @[]
+  matches: array[0.. MaxSubpatterns-1, string]
   line: TaintedString
+
+proc fill[I,T](arr: var array[I, T], val: T) =
+  for i in 0.. len(arr) - 1:
+    arr[i] = val
 
 for file in filenames:
   if not fileExists(file):
@@ -15,6 +19,13 @@ for file in filenames:
     quit(1)
   var fh = open(file, fmRead)
   while readLine(fh, line):
-    if contains(line, p):
-      echo line
+    fill(matches, nil)
+    if contains(line, p, matches):
+      for i, m in pairs(matches):
+        if m != nil:
+          echo m
+        elif i == 0:
+          echo line
+          break
+
   close(fh)
